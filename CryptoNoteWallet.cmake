67a0cc35d0df31f9a1620a051871476f5819e67c
neptuniumteam <mavrinevan@gmail.com>
2016-03-08 03:19:19 -0500

    v0.2.0.1

---
+++ CryptoNoteWallet.cmake
@@ -0,0 +1,4 @@
+
+set(CN_PROJECT_NAME "Neptunium")
+set(CN_CURRENCY_DISPLAY_NAME "Neptunium Testnet")
+set(CN_CURRENCY_TICKER "NPT")
---
+++ cryptonote/src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x92, 0xeb, 0xa9, 0xea, 0x87, 0xe5, 0x99, 0x16, 0x17, 0x39, 0x7d, 0x87, 0x2d, 0xd2, 0x0b, 0xcc } };
+}
